c9189123999c4811f01ff662e3acbde4eccf1c6c
rthdatasolutions <robert.honeybul@rthdatasolutions.com>
2017-01-30 18:32:22 +0800

    app and scraping altered

--- Bet/app.R
+++ Bet/app.R
@@ -7,61 +7,27 @@ if(!dir.exists('Data')){
   dir.create()
 }
 
-# Define UI for application that draws a histogram
 ui <- shinyUI(fluidPage(
    
-   # Application title
     actionButton("run", "Run Code"),
     textOutput("text1")
     
 ))
 
-# Define server logic required to draw a histogram
 server <- shinyServer(function(input, output) {
-  
-  # ii <- 1
-  # jj <- 1
-  # while(ii < 1000){
-  #   if(as.numeric(format(Sys.time(),'%M')) < 40){
   
   observeEvent(input$run, {
     
-    cat('Running Code')
-    url <- 'http://www.sportsbet.com.au/live-betting'
-    cat('Getting URL', file = stderr())
-    html <- read_html(url)
-    cat('Getting Nodes', file = stderr())
-    htmlNodes <- html_nodes(html,'td')
-    cat('Getting Text', file = stderr())
-    htmlText <- html_text(htmlNodes)
-    htmlText <- gsub('\n|\t|^\\s+','',htmlText[1:(grep('\r',htmlText)[1]-1)])
-    htmlText <- htmlText[-which(htmlText == "")]
-    df <- data.frame(htmlText[1:20],format(Sys.time(),'%X'))
-    colnames(df) <- c('Entry','Time')
-    cat('Writing CSV')
-
-    write.csv(df,paste0('Data/',format(Sys.time(),"%X"),'.csv'))
+    
+    source('scrape-script.R')
     
     output$text1 <- renderText({ 
-      paste0(list.files('Data'),'Finished')
+      paste('Finished',Sys.time())
     })
   })
-  
-  #  output$text1 <- renderText({ 
-  #    files <- list.files("Data")
-  #    file <- read.csv(paste0('Data/',files[1]))
-           
-  #    as.character(file$Entry[3])
-  #  })
-        
-        # cat(jj)
-        # jj <- jj + 1
-    #   } else{
-    #     next()
-    #   }
-    # }
+
 
 })
-# Run the application 
+
 shinyApp(ui = ui, server = server)
 
--- Bet/scrape-script.R
+++ Bet/scrape-script.R
@@ -2,17 +2,61 @@ library('rvest')
 
 setwd("~/Desktop/RTH/Clients/Shiny-Server/Bet")
 
-url <- 'http://www.sportsbet.com.au/live-betting'
+# url <- 'http://www.sportsbet.com.au/live-betting'
+url <- "365/Index/index.html"
 
 html <- read_html(url)
-htmlNodes <- html_nodes(html,'td')
-htmlText <- html_text(htmlNodes)
+htmlNodes <- html_nodes(html,'#basketball-us a')
 
+if(length(htmlNodes) == 0){
+  stop('NO US GAMES')
+}
 
-htmlText <- gsub('\n|\t|^\\s+','',htmlText[1:(grep('\r',htmlText)[1]-1)])
-htmlText <- htmlText[-which(htmlText == "")]
+HREFS <- html_attr(htmlNodes,'href')
+TITLES <- html_attr(htmlNodes,'title')
 
-df <- data.frame(htmlText[1:20],format(Sys.time(),'%X'))
-colnames(df) <- c('Entry','Time')
+to_remove <- c(which(is.na(TITLES)),grep('^Show all ',TITLES),grep(' more Markets$',TITLES))
 
-write.csv(df,paste0("Data/",format(Sys.time(),'%X'),'.csv'),row.names = F)
+HREFS <- HREFS[-to_remove]
+TITLES <- TITLES[-to_remove]
+
+overs = c()
+points = c()
+period = c()
+for(tt in 1:length(TITLES)){
+  
+  page <- paste0('365/Games/',TITLES[tt],'.html')
+  # page <- HREFS[tt]
+  html <- read_html(page)
+  htmlNodes <- html_nodes(html,'.title.left')
+  htmlTitles <- html_text(htmlNodes)
+  
+  points[tt] <- as.numeric(html_text(html_nodes(html,'#total-pts_TEAM_2'))) + as.numeric(html_text(html_nodes(html,'#total-pts_TEAM_1')))
+  period[tt] <- gsub('^Quarter ','Q',html_text(html_nodes(html,'#period_text')))
+  
+  over <- ""
+  if("Total Points Scored" %in% htmlTitles){
+    td <- html_nodes(html,'td')
+    td_text <- html_text(td)
+    td_over <- gsub('Over ','',grep('^Over ',td_text,value = T)[1])
+    over <- td_over
+  } else{
+    over <- 'NA' 
+  }
+  overs[tt] <- over
+}
+
+TITLES <- paste(TITLES," - ",format(as.POSIXlt(Sys.time(), "America/New_York"),"%a %b %d"))
+
+for(tt in 1:length(TITLES)){
+  df_title <- data.frame(format(as.POSIXlt(Sys.time(), "Australia/Perth"),"%X"),period[tt],points[tt],overs[tt])
+  colnames(df_title) <- c('Time','Period','Points','Line')
+  if(!(TITLES[tt] %in% gsub('.csv$','',list.files('Data')))){
+    write.csv(df_title,paste0("Data/",TITLES[tt],".csv"),row.names = F)
+  } else{
+    df_tt <- read.csv(paste0('Data/',TITLES[tt],'.csv'))
+    df_title <- rbind(df_tt,df_title)
+    write.csv(df_title,paste0("Data/",TITLES[tt],".csv"),row.names = F)
+  }
+}
+
