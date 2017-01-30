08a2cf69f9d900dcc4cdc751aa68f15c4bcf093e
rthdatasolutions <robert.honeybul@rthdatasolutions.com>
2017-01-30 15:02:53 +0800

    dfd

--- Bet/app.R
+++ Bet/app.R
@@ -7,7 +7,8 @@ options(warn = -1)
 ui <- shinyUI(fluidPage(
    
    # Application title
-    actionButton("run", "Run Code")
+    actionButton("run", "Run Code"),
+    textOutput("text1")
 ))
 
 # Define server logic required to draw a histogram
@@ -18,16 +19,25 @@ server <- shinyServer(function(input, output) {
     # while(ii < 1000){
     #   if(as.numeric(format(Sys.time(),'%M')) < 40){
     observeEvent(input$run, {
-      cat('Running Code')
-      url <- 'http://www.sportsbet.com.au/live-betting'
-      cat('Getting URL', file = stderr())
-      html <- read_html(url)
-      cat('Getting Nodes', file = stderr())
-      htmlNodes <- html_nodes(html,'td')
-      cat('Getting Text', file = stderr())
-      htmlText <- html_text(htmlNodes)
-      htmlText <- gsub('\n|\t|^\\s+','',htmlText[1:(grep('\r',htmlText)[1]-1)])
-      htmlText <- htmlText[-which(htmlText == "")]
+      
+      
+      
+      output$text1 <- renderText({ 
+        files <- list.files("Data")
+        file <- read.csv(paste0('Data/',files[1]))
+        
+        as.character(file$Entry[3])
+      })
+      # cat('Running Code')
+      # url <- 'http://www.sportsbet.com.au/live-betting'
+      # cat('Getting URL', file = stderr())
+      # html <- read_html(url)
+      # cat('Getting Nodes', file = stderr())
+      # htmlNodes <- html_nodes(html,'td')
+      # cat('Getting Text', file = stderr())
+      # htmlText <- html_text(htmlNodes)
+      # htmlText <- gsub('\n|\t|^\\s+','',htmlText[1:(grep('\r',htmlText)[1]-1)])
+      # htmlText <- htmlText[-which(htmlText == "")]
 
       # df <- data.frame(htmlText[1:20],format(Sys.time(),'%X'))
       # colnames(df) <- c('Entry','Time')
