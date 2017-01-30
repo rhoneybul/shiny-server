620a4ce28b345ffb99317f84caa1605d774eb765
rthdatasolutions <robert.honeybul@rthdatasolutions.com>
2017-01-30 14:50:18 +0800

    jdkf

--- Bet/app.R
+++ Bet/app.R
@@ -1,6 +1,5 @@
 library(shiny)
 library(rvest)
-library(DT)
 
 options(warn = -1)
 
@@ -14,30 +13,36 @@ ui <- shinyUI(fluidPage(
 # Define server logic required to draw a histogram
 server <- shinyServer(function(input, output) {
     
-    ii <- 1
-    jj <- 1
-    while(ii < 1000){
-      if(as.numeric(format(Sys.time(),'%M')) < 40){
-        url <- 'http://www.sportsbet.com.au/live-betting'
+    # ii <- 1
+    # jj <- 1
+    # while(ii < 1000){
+    #   if(as.numeric(format(Sys.time(),'%M')) < 40){
+    observeEvent(input$run, {
+      cat('Running Code')
+      url <- 'http://www.sportsbet.com.au/live-betting'
+      cat('Getting URL')
+      html <- read_html(url)
+      cat('Getting Nodes')
+      htmlNodes <- html_nodes(html,'td')
+      cat('Getting Text')
+      htmlText <- html_text(htmlNodes)
+      htmlText <- gsub('\n|\t|^\\s+','',htmlText[1:(grep('\r',htmlText)[1]-1)])
+      htmlText <- htmlText[-which(htmlText == "")]
+
+      df <- data.frame(htmlText[1:20],format(Sys.time(),'%X'))
+      colnames(df) <- c('Entry','Time')
+      cat('Writing CSV')
+      write.csv(df,paste0("Data/",format(Sys.time(),'%X'),'.csv'),row.names = F) 
+      cat("Finished")
+    })
         
-        html <- read_html(url)
-        htmlNodes <- html_nodes(html,'td')
-        htmlText <- html_text(htmlNodes)
-        
-        htmlText <- gsub('\n|\t|^\\s+','',htmlText[1:(grep('\r',htmlText)[1]-1)])
-        htmlText <- htmlText[-which(htmlText == "")]
-        
-        df <- data.frame(htmlText[1:20],format(Sys.time(),'%X'))
-        colnames(df) <- c('Entry','Time')
-        
-        write.csv(df,paste0("Data/",format(Sys.time(),'%X'),'.csv'),row.names = F) 
-        print(jj)
-        jj <- jj + 1
-      } else{
-        next()
-      }
-    }
-    
+        # cat(jj)
+        # jj <- jj + 1
+    #   } else{
+    #     next()
+    #   }
+    # }
+    # 
 
 })
 # Run the application 
