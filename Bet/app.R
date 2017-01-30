9eb46b77d21a04cb6077935fecfdc5beabc9a8c3
rthdatasolutions <robert.honeybul@rthdatasolutions.com>
2017-01-30 14:27:48 +0800

    changed

--- Bet/app.R
+++ Bet/app.R
@@ -15,7 +15,7 @@ server <- shinyServer(function(input, output) {
     ii <- 1
     jj <- 1
     while(ii < 1000){
-      if(format(Sys.time(),'%M') == '20'){
+      if(format(Sys.time(),'%M') == '30'){
         url <- 'http://www.sportsbet.com.au/live-betting'
         
         html <- read_html(url)
