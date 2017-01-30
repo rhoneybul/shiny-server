26d55e148306af269470681b152d1e6e0ddd4d2c
rthdatasolutions <robert.honeybul@rthdatasolutions.com>
2017-01-30 17:08:50 +0800

    changed app.R to include the scarping of sportsbet

--- Bet/app.R
+++ Bet/app.R
@@ -4,7 +4,7 @@ library(rvest)
 options(warn = -1)
 
 if(!dir.exists('Data')){
-  dir.create('Data')
+  dir.create()
 }
 
 # Define UI for application that draws a histogram
@@ -19,47 +19,40 @@ ui <- shinyUI(fluidPage(
 # Define server logic required to draw a histogram
 server <- shinyServer(function(input, output) {
   
-    # ii <- 1
-    # jj <- 1
-    # while(ii < 1000){
-    #   if(as.numeric(format(Sys.time(),'%M')) < 40){
-    observeEvent(input$run, {
-      
-      
-      output$text1 <- renderText({ 
-            list.files('Data')
-          })
-      
-      #  output$text1 <- renderText({ 
-      #    files <- list.files("Data")
-      #    file <- read.csv(paste0('Data/',files[1]))
-           
-      #    as.character(file$Entry[3])
-      #  })
-      
-      a = c(1,2,2)
-      b = c(4,5,6)
-      df <- data.frame(a,b)
-      
-      write.csv(df,paste0('Data/',format(Sys.time(),"%X"),'.csv'))
-      
-      # cat('Running Code')
-      # url <- 'http://www.sportsbet.com.au/live-betting'
-      # cat('Getting URL', file = stderr())
-      # html <- read_html(url)
-      # cat('Getting Nodes', file = stderr())
-      # htmlNodes <- html_nodes(html,'td')
-      # cat('Getting Text', file = stderr())
-      # htmlText <- html_text(htmlNodes)
-      # htmlText <- gsub('\n|\t|^\\s+','',htmlText[1:(grep('\r',htmlText)[1]-1)])
-      # htmlText <- htmlText[-which(htmlText == "")]
+  # ii <- 1
+  # jj <- 1
+  # while(ii < 1000){
+  #   if(as.numeric(format(Sys.time(),'%M')) < 40){
+  
+  observeEvent(input$run, {
+    
+    cat('Running Code')
+    url <- 'http://www.sportsbet.com.au/live-betting'
+    cat('Getting URL', file = stderr())
+    html <- read_html(url)
+    cat('Getting Nodes', file = stderr())
+    htmlNodes <- html_nodes(html,'td')
+    cat('Getting Text', file = stderr())
+    htmlText <- html_text(htmlNodes)
+    htmlText <- gsub('\n|\t|^\\s+','',htmlText[1:(grep('\r',htmlText)[1]-1)])
+    htmlText <- htmlText[-which(htmlText == "")]
+    df <- data.frame(htmlText[1:20],format(Sys.time(),'%X'))
+    colnames(df) <- c('Entry','Time')
+    cat('Writing CSV')
 
-      # df <- data.frame(htmlText[1:20],format(Sys.time(),'%X'))
-      # colnames(df) <- c('Entry','Time')
-      # cat('Writing CSV')
-      # write.csv(df,paste0("Data/",format(Sys.time(),'%X'),'.csv'),row.names = F) 
-      # cat("Finished")
+    write.csv(df,paste0('Data/',format(Sys.time(),"%X"),'.csv'))
+    
+    output$text1 <- renderText({ 
+      paste0(list.files('Data'),'Finished')
     })
+  })
+  
+  #  output$text1 <- renderText({ 
+  #    files <- list.files("Data")
+  #    file <- read.csv(paste0('Data/',files[1]))
+           
+  #    as.character(file$Entry[3])
+  #  })
         
         # cat(jj)
         # jj <- jj + 1
