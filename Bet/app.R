6945925b0982d3da796c4cf87511f1be4d743956
rthdatasolutions <robert.honeybul@rthdatasolutions.com>
2017-02-03 09:10:08 +0800

    Updated csv Files etc

--- Bet/app.R
+++ Bet/app.R
@@ -15,8 +15,6 @@ ui <- shinyUI(fluidPage(
     tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
   ),
   
-  
-  # tags$h1('Over/Under Tracker',style = 'text-align:center'),
   tags$h4('Select Game',style = 'text-align:center;padding-top:20px'),
   HTML("<center>"),
   selectInput('game',label = NULL,choices = data.files),
@@ -37,7 +35,8 @@ ui <- shinyUI(fluidPage(
            tags$h4(textOutput("text5")),
            tags$h4(textOutput("text6")),
            tags$h4(textOutput("text7")),
-           tags$h4(textOutput("text8"))
+           tags$h4(textOutput("text8")),
+           tags$h4(textOutput("text9"))
            )
   ),
   
@@ -56,13 +55,50 @@ server <- shinyServer(function(input, output) {
   
   observeEvent(input$run, {
     
+    url <- 'http://www.sportsbet.com.au/live-betting'
+    
+    html <- read_html(url)
+    htmlNodes <- html_nodes(html,'#basketball-us a')
+    
+    HREFS <- html_attr(htmlNodes,'href')
+    TITLES <- html_attr(htmlNodes,'title')
+    
+    to_remove <- c(which(is.na(TITLES)),grep('^Show all ',TITLES),grep(' more Markets$',TITLES))
+    
+    HREFS <- HREFS[-to_remove]
+    TITLES <- TITLES[-to_remove]
+    
+    withProgress(message = 'Getting Data',value = 0, {
+        source('scrape-script.R')
+    })
+    
     last_up <-format(Sys.time(),"%X")
     
     gameid <- as.character(input$game)
     
     game_data <- read.csv(paste0('Data/',gameid))
     
-    game_data$GameTime <- gsub("Half ","H",game_data$GameTime)
+    game_data$GameTime <- gsub(" 1st"," Q1",game_data$GameTime)
+    game_data$GameTime <- gsub("\\ 2nd"," Q2",game_data$GameTime)
+    game_data$GameTime <- gsub(" 3rd", " Q3",game_data$GameTime)
+    game_data$GameTime <- gsub(" 4th"," Q4",game_data$GameTime)
+    game_data$GameTime <- gsub("1st ","H1 ",game_data$GameTime)
+    game_data$GameTime <- gsub("2nd ","H2 ",game_data$GameTime)
+    game_data$GameTime <- gsub('^:','0:',game_data$GameTime)
+    game_data$GameTime <- gsub('\\.*','',game_data$GameTime)
+    game_data$GameTime <- gsub('End','0:00 ',game_data$GameTime)
+    game_data$GameTime <- gsub('Halftime','H1 0:00',game_data$GameTime)
+    game_data$GameTime <- gsub('Half','0:00 Q2',game_data$GameTime)
+    
+    
+    if(game_data$Type[1] == 'NCAA'){
+      for(jj in 1:nrow(game_data)){
+        g_ii <- strsplit(game_data$GameTime[jj]," ")[[1]]
+        if(length(g_ii) > 1){
+          game_data$GameTime[jj] <- paste(strsplit(game_data$GameTime[jj]," ")[[1]][2:1],collapse = " ")
+        }
+      }
+    }
     
     if(game_data$Type[1] == 'NCAA'){
       college_times <- 1:20
@@ -76,15 +112,14 @@ server <- shinyServer(function(input, output) {
       
       points_df <- data.frame(game_times,NA,NA,NA)
       colnames(points_df) <- c("GameTime","Points","PointsInMinute","Line")
-      points_df$Points[1] <- 0
-      points_df$Line[1] <- game_data$Line[1]
-      for(ii in 2:nrow(points_df)){
+
+      for(ii in 1:nrow(points_df)){
         per <- strsplit(as.character(points_df$GameTime[ii])," - ")[[1]][1]
         min <-  as.character(as.numeric(gsub("m","",strsplit(as.character(points_df$GameTime)[ii]," - ")[[1]][2]))-1)
         
-        if(length(intersect(grep(per,game_data$GameTime),grep(paste0(" ",min,":"),game_data$GameTime))) > 0){
-          points_ii <- game_data$Points[intersect(grep(per,game_data$GameTime),grep(paste0(" ",min,":"),game_data$GameTime))]
-          line_ii <- game_data$Line[intersect(grep(per,game_data$GameTime),grep(paste0(" ",min,":"),game_data$GameTime))]
+        if(length(intersect(grep(per,game_data$GameTime),grep(paste0("^",min,":"),game_data$GameTime))) > 0){
+          points_ii <- game_data$Points[intersect(grep(per,game_data$GameTime),grep(paste0("^",min,":"),game_data$GameTime))]
+          line_ii <- game_data$Line[intersect(grep(per,game_data$GameTime),grep(paste0("^",min,":"),game_data$GameTime))]
         } else{
           points_ii <- NA
           line_ii <- NA
@@ -93,20 +128,21 @@ server <- shinyServer(function(input, output) {
         points_df$Points[ii] <- points_ii
         points_df$Line[ii] <- line_ii
         
-      }
+     }
       
-      points_in_minute <- 0
-      for(pp in 2:nrow(points_df)){
+     points_in_minute <- c()
+     for(pp in 1:nrow(points_df)){
         points_in_minute[pp] <- points_df$Points[pp] - points_df$Points[pp-1]
-      }
+     }
       
-      points_df$PointsInMinute <- points_in_minute
+     points_df$PointsInMinute <- points_in_minute
+     
+     points_df <<- points_df
       
-      points_df <<- points_df
-      
-      print('points')
+     print('points')
       
     }
+    
     if(game_data$Type[1] == "NBA"){
       game_times <- c()
       nba_times <- 1:12
@@ -119,15 +155,14 @@ server <- shinyServer(function(input, output) {
       
       points_df <- data.frame(game_times,NA,NA,NA)
       colnames(points_df) <- c("GameTime","Points","PointsInMinute","Line")
-      points_df$Points[1] <- 0
-      points_df$Line[1] <- game_data$Line[1]
-      for(ii in 2:nrow(points_df)){
+      
+      for(ii in 1:nrow(points_df)){
         per <- strsplit(as.character(points_df$GameTime[ii])," - ")[[1]][1]
         min <-  as.character(as.numeric(gsub("m","",strsplit(as.character(points_df$GameTime)[ii]," - ")[[1]][2]))-1)
         
-        if(length(intersect(grep(per,game_data$GameTime),grep(paste0(" ",min,":"),game_data$GameTime))) > 0){
-          points_ii <- game_data$Points[intersect(grep(per,game_data$GameTime),grep(paste0(" ",min,":"),game_data$GameTime))]
-          line_ii <- game_data$Line[intersect(grep(per,game_data$GameTime),grep(paste0(" ",min,":"),game_data$GameTime))]
+        if(length(intersect(grep(per,game_data$GameTime),grep(paste0("^",min,":"),game_data$GameTime))) > 0){
+          points_ii <- game_data$Points[intersect(grep(per,game_data$GameTime),grep(paste0("^",min,":"),game_data$GameTime))]
+          line_ii <- game_data$Line[intersect(grep(per,game_data$GameTime),grep(paste0("^",min,":"),game_data$GameTime))]
         } else{
           points_ii <- NA
           line_ii <- NA
@@ -138,8 +173,8 @@ server <- shinyServer(function(input, output) {
         
       }
       
-      points_in_minute <- 0
-      for(pp in 2:nrow(points_df)){
+      points_in_minute <- c()
+      for(pp in 1:nrow(points_df)){
         points_in_minute[pp] <- points_df$Points[pp] - points_df$Points[pp-1]
       }
       
@@ -151,10 +186,23 @@ server <- shinyServer(function(input, output) {
     points_df_na <- points_df
     
     output$plot1 <- renderPlot({
-      plot(points_df_na$Line,type = "l",xlab = "Time",ylab = "Line",col = 'red')
+      if(length(which(!is.na(points_df_na$Line))) > 5){
+        plot(points_df_na$Line,xlab = "Time",type = "o",ylab = "Line",col = 'red',axes = F)
+        axis(1,at = 1:length(points_df_na$Line),labels = points_df_na$GameTime,las = T)
+        axis(2,at = min(points_df_na$Line,na.rm = T):max(points_df_na$Line,na.rm = T),labels = min(points_df_na$Line,na.rm = T):max(points_df_na$Line,na.rm = T),las = T)
+        
+      } else{
+        plot(1:10,type = "n",axes = F,xlab = "",ylab = "")
+        text(5,7,paste("Not Enough Data Yet..",round(length(which(!is.na(points_df_na$Line))) / 5,digits = 2) * 100,"% Gathered."),cex = 1.2)
+      }
     })
     output$plot2 <- renderPlot({
-      barplot(points_df_na$PointsInMinute,col = 'blue',ylab = "Points Per Minute",xlab = "Time")
+      if(length(which(!is.na(points_df_na$Line))) > 5){
+        barplot(points_df_na$PointsInMinute,names.arg = gsub('m','',gsub(' - ','-',points_df_na$GameTime)),col = 'blue',ylab = "Points Per Minute",xlab = "Time")
+      } else {
+        plot(1:10,type = "n",axes = F,xlab = "",ylab = "")
+        text(5,7,paste("Not Enough Data Yet..",round(length(which(!is.na(points_df_na$Line))) / 5,digits = 2) * 100,"% Gathered."),cex = 1.2)
+      }
     })
     
     output$table <- renderTable(points_df)
@@ -167,27 +215,61 @@ server <- shinyServer(function(input, output) {
     curr_line <- game_data$Line[nrow(game_data)]
     curr_points <- game_data$Points[nrow(game_data)]
     curr_gametime <- game_data$GameTime[nrow(game_data)]
-
-    if(any(is.na(points_df$Line))){
-      n_na <- length(which(is.na(points_df$Line)))
-      points_df <- points_df[-which(is.na(points_df$Line)),]
-      
-      curr_ppm <- round(mean(points_df$PointsInMinute),digits = 2)
-      
-      total_at_curr <- round(curr_points + curr_ppm * n_na,digits = 0)
-      req_rate <- round((curr_line-curr_points) / n_na,digits = 2)
+    
+    if(length(curr_gametime) == 0 || length(curr_points) == 0 || length(curr_line) == 0){
+      curr_line <- "NA"
+      curr_points <- "NA"
+      curr_gametime <- "NA"
+      time.left <- "NA"
+      mins.left <- "NA"
+      max.points <- "NA"
+      curr_ppm <- "NA"
+      total_at_curr <- "NA"
+      req_rate <- "NA"
     } else{
-      curr_ppm <- round(mean(points_df$PointsInMinute),digits = 2)
-      total_at_curr <- NA
-      req_rate <- NA
+      if(game_data$Type[1] == 'NCAA'){
+        mins.left <- strsplit(strsplit(curr_gametime," ")[[1]][1],':')[[1]][1]
+        if(length(grep('H2',curr_gametime)) > 0){
+          time.left <- as.numeric(mins.left)
+        } else{
+          time.left <- 20 + as.numeric(mins.left)
+        }
+      } else{
+        mins.left <- strsplit(strsplit(curr_gametime," ")[[1]][1],':')[[1]][1]
+        if(mins.left == "Half"){
+          
+        }
+        if(length(grep('Q4',curr_gametime)) > 0){
+          time.left <- as.numeric(mins.left)
+        } else{
+          if(length(grep('Q3',curr_gametime)) > 0){
+            time.left <- 12 + as.numeric(mins.left)
+          } else{
+            if(length(grep('Q2',curr_gametime)) > 0){
+              time.left <- 24 + as.numeric(mins.left)
+            } else{
+              if(length(grep('Q1',curr_gametime)) > 0){
+                time.left <- 36 + as.numeric(mins.left)
+              } 
+            }
+          }
+        }
+      }
+      
+      time.left <- as.numeric(time.left)
+      
+      max.points <- max(points_df$Points,na.rm = T)
+      
+      curr_ppm <-  round(max.points / max(which(points_df$Points == max.points)),digits = 2)
+      total_at_curr <- round(curr_points + curr_ppm * time.left,digits = 0)
+      req_rate <- round((curr_line-curr_points) / time.left,digits = 2)
     }
-    
-    
-    
-    # source('scrape-script.R')
-    
+
     output$text6 <- renderText({ 
       paste0("Game: ",strsplit(gameid," - ")[[1]][1])
+    })
+    output$text9 <- renderText({ 
+      paste0("Minutes Left: ",time.left)
     })
     output$text1 <- renderText({ 
       paste0("Line: ", curr_line)
--- Bet/scrape-script.R
+++ Bet/scrape-script.R
@@ -1,22 +1,147 @@
 library('rvest')
 library('RSelenium')
 
-url <- 'http://www.sportsbet.com.au/live-betting'
+## FUNCTIONS
 
-html <- read_html(url)
-htmlNodes <- html_nodes(html,'#basketball-us a')
+basket_scrape = function() {
+  
+  overs = c()
+  win_1 <- c()
+  win_2 <- c()
+  hand <- c()
+  points = c()
+  period = c()
+  game_type <- c()
+  game_time <- c()
+  if(length(TITLES) > 0){
+    for(tt in 1:length(TITLES)){
+      
+      prog_amount <- tt / length(TITLES)
+      
+      incProgress(amount = prog_amount)
+      
+      ## GET TIME ##
+      
+      title_comp_ii <- strsplit(gsub('\\s+$','',gsub(' At | - .*',' ',TITLES[tt]))," ")[[1]]
+      
+      game_type[tt] <- ''
+      if(length(grep(paste(title_comp_ii,collapse = "|"),scoreHTMLnba)) > 0){
+        
+        game_type[tt] <- 'NBA'
+        
+        game_ii <- min(grep(paste(title_comp_ii,collapse = "|"),scoreHTMLnba))-1
+        
+        if(game_ii == 0){
+          game_time[tt] <- 'NA'
+        } else{
+          game_time[tt] <- scoreHTMLnba[game_ii]
+          
+          if(game_time[tt] %in% title_comp || game_time[tt] == "Final" ){
+            game_time[tt] <- "NA"
+            
+          }
+        }
+        
+      } else{
+        
+        game_type[tt] <- 'NCAA'
+        
+        greps <- c()
+        for(tit in title_comp_ii){
+          greps <- c(greps,grep(tit,scoreHTMLncaa))
+        }
+        
+        gTab <- table(greps)
+        
+        if(max(gTab) == 1 || length(greps) == 0){
+          game_time[tt] <- "NA"
+        } else{
+          grep_ii <- names(gTab)[which(as.numeric(gTab) == max(as.numeric(gTab)))]
+          
+          game_time[tt] <- scoreHTMLncaa[as.numeric(grep_ii) + 1]
+          
+        }
+      }
+      
+      page <- HREFS[tt]
+      html <- read_html(page)
+      htmlNodes <- html_nodes(html,'.title.left,.date')
+      htmlTitles <- html_text(htmlNodes)
+      
+      if(length(as.numeric(html_text(html_nodes(html,'#total-pts_TEAM_2'))) + as.numeric(html_text(html_nodes(html,'#total-pts_TEAM_1')))) != 0){
+        points[tt] <- as.character(as.numeric(html_text(html_nodes(html,'#total-pts_TEAM_2'))) + as.numeric(html_text(html_nodes(html,'#total-pts_TEAM_1'))))
+      } else{
+        if(length(as.numeric(html_text(html_nodes(html,'.team-score')))) != 0){
+          points[tt] <- as.character(sum(as.numeric(html_text(html_nodes(html,'.team-score')))))
+        } else{
+          points[tt] <- 'NA'
+        }
+      }
+      
+      if(length(html_text(html_nodes(html,'#period_text'))) > 0){
+        period[tt] <- gsub('^Quarter ','Q',html_text(html_nodes(html,'#period_text')))
+      } else{
+        period[tt] <- 'NA'
+      }  
+      
+      over <- ""
+      if("Total Points Scored" %in% htmlTitles || "Total Points" %in% htmlTitles){
+        td <- html_nodes(html,'td')
+        td_text <- html_text(td)
+        td_over <- gsub('^Over ','',td_text[grep('^Over ',td_text)[1]])
+        over <- td_over
+      } else{
+        over <- 'NA' 
+      }
+      overs[tt] <- over
+      
+      win_1[tt] <- ''
+      win_2[tt] <- ''
+      if('Match' %in% htmlTitles){
+        td_match <- html_nodes(html,'td')
+        if(length(td_match) != 0){
+          match_text <- html_text(td_match)
+          match_ii <- grep(paste(title_comp_ii,collapse = "|"),match_text,value = T)
+          match_ii <- grep("\n",match_ii,value = T)
+          
+          w1 <- strsplit(match_ii[1]," ")[[1]]
+          w2 <- strsplit(match_ii[2]," ")[[1]]
+          win_1[tt] <- strsplit(w1[length(w1)],"\r")[[1]][1]
+          win_2[tt] <- strsplit(w2[length(w2)],"\r")[[1]][1]
+        }
+      }
+      
+      hand[tt] <- ''
+      if('Handicap Betting' %in% htmlTitles || 'Handicap' %in% htmlTitles){
+        td <- html_nodes(html,'td')
+        hand_text <- html_text(td)
+        hand_ii <- grep('\\+',hand_text,value = T)
+        hand_ii <- grep('\n',hand_ii,invert = T,value = T)
+        hand_te <- gsub('\\+','',gsub('..* +','',hand_ii))[1]
+        
+        hand[tt] <- hand_te
+      }
+      
+      print(tt)
+    }
+    
+    TITLES <- paste(TITLES," - ",format(as.POSIXlt(Sys.time(), "Australia/Perth"),"%a %b %d"))
+    
+    for(tt in 1:length(TITLES)){
+      df_title <- data.frame(format(as.POSIXlt(Sys.time(), "Australia/Perth"),"%X"),game_time[tt],period[tt],points[tt],overs[tt],win_1[tt],win_2[tt],hand[tt],game_type[tt])
+      colnames(df_title) <- c('Time','GameTime','Period','Points','Line','MatchAway','MatchHome','HandicapLine','Type')
+      if(!(TITLES[tt] %in% gsub('.csv$','',list.files('Data')))){
+        write.csv(df_title,paste0("Data/",TITLES[tt],".csv"),row.names = F)
+      } else{
+        df_tt <- read.csv(paste0('Data/',TITLES[tt],'.csv'))
+        df_title <- rbind(df_tt,df_title)
+        write.csv(df_title,paste0("Data/",TITLES[tt],".csv"),row.names = F)
+      }
+    }
+  }
+}
 
-if(length(htmlNodes) != 0){
-  HREFS <- html_attr(htmlNodes,'href')
-  TITLES <- html_attr(htmlNodes,'title')
-  
-  to_remove <- c(which(is.na(TITLES)),grep('^Show all ',TITLES),grep(' more Markets$',TITLES))
-  
-  HREFS <- HREFS[-to_remove]
-  TITLES <- TITLES[-to_remove]
-  
-  title_comp <- strsplit(gsub('\\s+$','',gsub(' At | - .*',' ',paste(TITLES,collapse = " ")))," ")[[1]]
-  
+get_nba= function(){
   espn <- 'https://sports.yahoo.com/nba/scoreboard/'
   
   espn <- read_html(espn)
@@ -25,94 +150,38 @@ if(length(htmlNodes) != 0){
   
   scoreHTMLnba <- scoreHTMLnba[5:length(scoreHTMLnba)]
   scoreHTMLnba <- scoreHTMLnba[-which(scoreHTMLnba == "")]
-  
+}
+
+get_ncaa = function(){
   ncaa <- 'http://jsonline.sportsdirectinc.com/sports-scores/College-Basketball-Scores-Matchups.aspx'
   
   ncaa <- read_html(ncaa)
   ncaa <- html_nodes(ncaa,'.sdi-so-title , .sdi-datahead-sub')
   scoreHTMLncaa <- html_text(ncaa)
+  return(scoreHTMLncaa)
+}
+
+title_comp <- strsplit(gsub('\\s+$','',gsub(' At | - .*',' ',paste(TITLES,collapse = " ")))," ")[[1]]
+
+scoreHTMLnba <- get_nba()
+
+while(length(scoreHTMLnba) <= 1){
+  print('nba failed once')
+  scoreHTMLnba <- get_nba()
+}
+
+scoreHTMLncaa <- get_ncaa()
+while(length(scoreHTMLncaa) == 0){
+  print('NCAA failed once')
+  scoreHTMLncaa <- get_ncaa()
+}
+
+if(length(TITLES) != 0 || length(htmlNodes) != 0){
   
-  overs = c()
-  points = c()
-  period = c()
-  game_times <- c()
-  for(tt in 1:length(TITLES)){
-    
-    ## GET TIME ##
-    
-    title_comp_ii <- strsplit(gsub('\\s+$','',gsub(' At | - .*',' ',TITLES[tt]))," ")[[1]]
-    
-    if(length(grep(paste(title_comp_ii,collapse = "|"),scoreHTMLnba)) > 0){
-      game_time[tt] <- scoreHTMLnba[min(grep(paste(title_comp_ii,collapse = "|"),scoreHTMLnba))-1]
-      if(game_time[tt] %in% title_comp || game_time[tt] == "Final"){
-        game_time[tt] <- "NA"
-      }
-    } else{
-      
-      greps <- c()
-      for(tit in title_comp_ii){
-        greps <- c(greps,grep(tit,scoreHTMLncaa))
-      }
-      
-      if(max(gTab) == 1){
-        game_time[tt] <- "NA"
-      } else{
-        grep_ii <- names(gTab)[which(as.numeric(gTab) == max(as.numeric(gTab)))]
-        
-        game_time[tt] <- scoreHTMLncaa[as.numeric(grep_ii) + 1]
-        
-      }
-    }
-    
-    page <- HREFS[tt]
-    html <- read_html(page)
-    htmlNodes <- html_nodes(html,'.title.left')
-    htmlTitles <- html_text(htmlNodes)
-    
-    if(length(as.numeric(html_text(html_nodes(html,'#total-pts_TEAM_2'))) + as.numeric(html_text(html_nodes(html,'#total-pts_TEAM_1')))) != 0){
-      points[tt] <- as.character(as.numeric(html_text(html_nodes(html,'#total-pts_TEAM_2'))) + as.numeric(html_text(html_nodes(html,'#total-pts_TEAM_1'))))
-    } else{
-      if(length(as.numeric(html_text(html_nodes(html,'.team-score')))) != 0){
-        points[tt] <- as.character(sum(as.numeric(html_text(html_nodes(html,'.team-score')))))
-      } else{
-        points[tt] <- 'NA'
-      }
-    }
-    
-    if(length(html_text(html_nodes(html,'#period_text'))) > 0){
-      period[tt] <- gsub('^Quarter ','Q',html_text(html_nodes(html,'#period_text')))
-    } else{
-      period[tt] <- 'NA'
-    }  
-    
-    over <- ""
-    if("Total Points Scored" %in% htmlTitles || "Total Points" %in% htmlTitles){
-      td <- html_nodes(html,'td')
-      td_text <- html_text(td)
-      td_over <- gsub('Over ','',grep('^Over ',td_text,value = T)[1])
-      over <- td_over
-    } else{
-      over <- 'NA' 
-    }
-    overs[tt] <- over
-    
-    print(tt)
-  }
+    basket_scrape()
   
-  TITLES <- paste(TITLES," - ",format(as.POSIXlt(Sys.time(), "America/New_York"),"%a %b %d"))
-  
-  for(tt in 1:length(TITLES)){
-    df_title <- data.frame(format(as.POSIXlt(Sys.time(), "Australia/Perth"),"%X"),game_time[tt],period[tt],points[tt],overs[tt])
-    colnames(df_title) <- c('Time','GameTime','Period','Points','Line')
-    if(!(TITLES[tt] %in% gsub('.csv$','',list.files('Data')))){
-      write.csv(df_title,paste0("Data/",TITLES[tt],".csv"),row.names = F)
-    } else{
-      df_tt <- read.csv(paste0('Data/',TITLES[tt],'.csv'))
-      df_title <- rbind(df_tt,df_title)
-      write.csv(df_title,paste0("Data/",TITLES[tt],".csv"),row.names = F)
-    }
-  }
 } else{
   print("No US Games")
 }
+  
 
